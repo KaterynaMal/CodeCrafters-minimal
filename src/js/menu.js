3210a302d641d3eece0531ba75512997c4451cfb
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-08-14 20:46:32 +0300

    +-

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
