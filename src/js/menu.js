75a65185103579f484c001b40c427df9460d3c27
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-08-15 00:27:47 +0300

    correction js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.menu.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
   }
 })();
